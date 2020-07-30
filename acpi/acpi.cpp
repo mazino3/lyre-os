74c7eb52af6ccca83335f79f6b7b4a53001a9117
mintsuki <mintsuki@protonmail.com>
2020-07-30 15:33:37 +0200

    acpi: Add MADT parser

--- acpi/acpi.cpp
+++ acpi/acpi.cpp
@@ -3,6 +3,7 @@
 #include <lib/print.hpp>
 #include <lib/builtins.h>
 #include <acpi/acpi.hpp>
+#include <acpi/madt.hpp>
 #include <mm/vmm.hpp>
 
 struct RSDP {
@@ -33,22 +34,29 @@ void acpi_init(RSDP *rsdp) {
 
     if (rsdp->rev >= 2 && rsdp->xsdt_addr) {
         use_xsdt = true;
-        print("acpi: Found XSDT at %X\n", ((uintptr_t)rsdp->xsdt_addr + MEM_PHYS_OFFSET));
         rsdt = (RSDT *)((uintptr_t)rsdp->xsdt_addr + MEM_PHYS_OFFSET);
+        print("acpi: Found XSDT at %X\n", (uintptr_t)rsdt);
     } else {
         use_xsdt = false;
-        print("acpi: Found RSDT at %X\n", ((uintptr_t)rsdp->rsdt_addr + MEM_PHYS_OFFSET));
         rsdt = (RSDT *)((uintptr_t)rsdp->rsdt_addr + MEM_PHYS_OFFSET);
+        print("acpi: Found RSDT at %X\n", (uintptr_t)rsdt);
     }
+
+    // Initialised individual tables that need initialisation
+    init_madt();
 }
 
 /* Find SDT by signature */
 void *acpi_find_sdt(const char *signature, int index) {
     int cnt = 0;
-    size_t interval = use_xsdt ? 8 : 4;
 
-    for (size_t i = 0; i < rsdt->sdt.length - sizeof(SDT); i += interval) {
-        SDT *ptr = (SDT *)((uintptr_t)rsdt->ptrs_start + i + MEM_PHYS_OFFSET);
+    for (size_t i = 0; i < rsdt->sdt.length - sizeof(SDT); i++) {
+        SDT *ptr;
+        if (use_xsdt)
+            ptr = (SDT *)(((uint64_t *)rsdt->ptrs_start)[i] + MEM_PHYS_OFFSET);
+        else
+            ptr = (SDT *)(((uint32_t *)rsdt->ptrs_start)[i] + MEM_PHYS_OFFSET);
+
         if (!strncmp(ptr->signature, signature, 4) && cnt++ == index) {
             print("acpi: Found \"%s\" at %X\n", signature, ptr);
             return (void *)ptr;

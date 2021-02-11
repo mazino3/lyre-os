d6363dfb39781b208967c690c53f1734aeb84808
mintsuki <mintsuki@protonmail.com>
2021-02-11 03:14:46 +0100

    New font

--- kernel/lib/bitmap_font.h
+++ kernel/lib/bitmap_font.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+// Font binary from https://github.com/spacerace/romfont/blob/master/font-bin/seabios8x16.bin
+
 #define bitmap_font_width  8
 #define bitmap_font_height 16
 #define bitmap_font_glyphs 256

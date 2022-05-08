2210808d660388c15a931d04553899bc326b095e
mintsuki <mintsuki@protonmail.com>
2022-05-08 20:58:50 +0200

    Update Limine

--- make-iso.sh
+++ make-iso.sh
@@ -10,6 +10,6 @@ SYSROOT="$3"
 rm -rf pack
 mkdir -p pack/boot
 cp "$BUILD_DIR"/initramfs.tar "$SYSROOT"/boot/lyre.elf pack/
-cp "$SOURCE_DIR"/limine.cfg "$BUILD_DIR"/tools/host-limine/share/limine/limine.sys "$BUILD_DIR"/tools/host-limine/share/limine/limine-cd.bin "$BUILD_DIR"/tools/host-limine/share/limine/limine-eltorito-efi.bin pack/boot/
-xorriso -as mkisofs -b /boot/limine-cd.bin -no-emul-boot -boot-load-size 4 -boot-info-table --efi-boot /boot/limine-eltorito-efi.bin -efi-boot-part --efi-boot-image --protective-msdos-label pack -o lyre.iso
-"$BUILD_DIR"/tools/host-limine/bin/limine-install lyre.iso
+cp "$SOURCE_DIR"/limine.cfg "$BUILD_DIR"/tools/host-limine/share/limine/limine.sys "$BUILD_DIR"/tools/host-limine/share/limine/limine-cd.bin "$BUILD_DIR"/tools/host-limine/share/limine/limine-cd-efi.bin pack/boot/
+xorriso -as mkisofs -b /boot/limine-cd.bin -no-emul-boot -boot-load-size 4 -boot-info-table --efi-boot /boot/limine-cd-efi.bin -efi-boot-part --efi-boot-image --protective-msdos-label pack -o lyre.iso
+"$BUILD_DIR"/tools/host-limine/bin/limine-deploy lyre.iso
